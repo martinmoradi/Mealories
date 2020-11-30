05f7cea457184adb85a40ebd96d10cb77ef9e517
Uriel <uriel.sautron@gmail.com>
2020-11-30 18:29:44 +0100

    Config params devise and letter opener

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_divise_parameters, if: :devise_controller?
+ 
 
-  def configure_divise_parameters
-    devise_parameter_sanitizer.permit(:sing_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation)}
 end
