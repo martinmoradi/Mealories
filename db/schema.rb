1a2a9a620c1443b023a284d528a376fe1daef4ee
AxelVigroux <vix.ax@hotmail.fr>
2020-11-30 17:09:23 +0100

    Rails g model Menu. write in comment t.references :user... And add belongs_to :user in model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_131356) do
+ActiveRecord::Schema.define(version: 2020_11_30_160556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title", null: false
