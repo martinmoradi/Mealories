0952ccab0222ea355c6ba8d79cc34faccc3b3385
AxelVigroux <vix.ax@hotmail.fr>
2020-11-30 16:28:40 +0100

    update t.bigint to t.references :user (in comment atm) and remove null false in migrate create recipe model

--- db/migrate/20201130131356_create_recipes.rb
+++ db/migrate/20201130131356_create_recipes.rb
@@ -2,13 +2,13 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
       t.string :title, null:false
-      t.integer :servings, null:false
-      t.integer :preptime, null:false
-      t.integer :cooktime, null:false
-      t.text :steps, null:false
-      t.text :ingredients, null:false
-      t.string :image, null:false
-      t.bigint "author_id", default: 0
+      t.integer :servings
+      t.integer :preptime
+      t.integer :cooktime
+      t.text :steps
+      t.text :ingredients
+      t.string :image
+      #t.references :user, null: false, foreign_key: true
       t.integer :calories
       t.integer :total_daily
 
