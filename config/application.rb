99be868369105c283ba1089717ce4cfc31ebc053
Martin Moradi <moradi.martin@gmail.com>
2020-12-02 18:36:27 +0100

    add scrap_to_nutrients service

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Mealories
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_mailer.delivery_method = :mailjet_api
+  
+    # https://stackoverflow.com/questions/57277351/rails-6-zeitwerknameerror-doesnt-load-class-from-module
+    config.autoloader = :classic
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
