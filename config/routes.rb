996da8b736ae35fb806f952d26de9a2ab937ca45
AxelVigroux <vix.ax@hotmail.fr>
2020-12-03 10:13:33 +0100

    Connecting recipe_card

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :plans
     resources :days
     resources :users
+    resources :recipes
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
