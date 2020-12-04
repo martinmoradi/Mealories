bd6ba8b8f23650de8c8fa77e4ee9c474682e9f73
David <david.lobry96@gmail.com>
2020-12-04 11:28:49 +0100

    user show connected with real values and adding partials + edit

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -5,4 +5,14 @@ class Users::UsersController < Users::ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user =  current_user
+    user_params =  params.require(:user).permit(:first_name, :last_name, :email, :objective, :weight_in_kgs, :gender, :age, :height_in_cms, :activity_level)
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 end
