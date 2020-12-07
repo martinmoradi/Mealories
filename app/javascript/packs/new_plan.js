2bef556c5c4c73ce6c455e9dbb8f6316126e8fc1
Uriel <uriel.sautron@gmail.com>
2020-12-07 21:58:29 +0100

    Fix nb of days between 1 and 7

--- app/javascript/packs/new_plan.js
+++ app/javascript/packs/new_plan.js
@@ -6,6 +6,9 @@ const decrement = (e) => {
     e.preventDefault();
     let inputNumber = parseInt(input.value);
     inputNumber -= 1;
+    if (inputNumber < 1) {
+        inputNumber = 1
+    }
     input.value = inputNumber;
     return input.value
 };
@@ -13,13 +16,12 @@ const increment = (e) => {
     e.preventDefault();
     let inputNumber = parseInt(input.value);
     inputNumber += 1;
+    if (inputNumber > 7) {
+        inputNumber = 7
+    }
     input.value = inputNumber;
     return input.value;
 };
 
 btnDecrement.addEventListener("click", decrement);
 btnIncrement.addEventListener("click", increment);
-
-btnDecrement.addEventListener("click", e => {
-    console.log(e);
-});
