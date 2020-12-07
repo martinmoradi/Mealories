1a71aae1af3abc8d6af548d3ca15ac35443ce915
Uriel <uriel.sautron@gmail.com>
2020-12-07 21:21:41 +0100

    Ajax for destroy plan

--- app/javascript/packs/new_plan.js
+++ app/javascript/packs/new_plan.js
@@ -1,21 +1,23 @@
 const btnDecrement = document.querySelector("#decrement");
 const btnIncrement = document.querySelector("#increment");
-const input = document.querySelector("#plan_nb_of_days");
+const input = document.querySelector("#nb_of_days");
 
 const decrement = (e) => {
-  e.preventDefault();
-  let inputNumber = parseInt(input.value);
-  inputNumber -= 1;
-  input.value = inputNumber;
-  return input.value
+    e.preventDefault();
+    let inputNumber = parseInt(input.value);
+    inputNumber -= 1;
+    input.value = inputNumber;
+    return input.value
 };
 const increment = (e) => {
-  e.preventDefault();
-  let inputNumber = parseInt(input.value);
-  inputNumber += 1;
-  input.value = inputNumber;
-  return input.value;
+    e.preventDefault();
+    let inputNumber = parseInt(input.value);
+    inputNumber += 1;
+    input.value = inputNumber;
+    return input.value;
 };
 
 btnDecrement.addEventListener("click", decrement);
 btnIncrement.addEventListener("click", increment);
+
+console.log(eventListener("click"));
