31bb81a68fc225e104caba000a7b2a9b7243e32e
Uriel <uriel.sautron@gmail.com>
2020-12-09 21:12:52 +0100

    Flash alert devise

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -1,11 +1,11 @@
 class Users::ApplicationController < ApplicationController
   before_action :authenticate_user!
-  add_flash_types :info, :error, :warning
+  
 
   def user_profile_incomplete
     if current_user.incomplete_profile?
       redirect_to edit_user_path(current_user.id)
-      flash[:info] = "Ton profil n'est pas complet"
+      flash[:info]="Ton profil n'est pas complet"
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     {
       notice: 'bg-primary-600 border-l-4 border-primary-900 text-white',
       error:  'bg-red-400 border-l-4 border-red-700 text-black',
-      info:   'bg-yellow-300 border-l-4 border-yellow-400 text-black'
+      info:   'bg-yellow-300 border-l-4 border-yellow-400 text-black',
+      alert:   'bg-yellow-300 border-l-4 border-yellow-400 text-black'
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
@@ -11,7 +12,8 @@ module ApplicationHelper
     {
       notice: 'text-white',
       error:  'text-black',
-      info:   'text-black'
+      info:   'text-black',
+      alert:   'text-black'
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
