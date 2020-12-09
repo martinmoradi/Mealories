7736d84bc52f48d76af6fe5c9476af852528ddf9
Uriel <uriel.sautron@gmail.com>
2020-12-09 15:53:30 +0100

    Edit flash

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -1,10 +1,12 @@
 class Users::ApplicationController < ApplicationController
   before_action :authenticate_user!
   
+  add_flash_types :info, :error, :warning
+
 
   def user_profile_incomplete
     redirect_to edit_user_path(current_user.id) if current_user.incomplete_profile?
-    flash[:notice]="Ton profil n'est pas complet"
+    flash[:info]="Ton profil n'est pas complet"
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   def tailwind_classes_for(flash_type)
     {
-      notice: "bg-primary-500 border-l-4 border-primary-800 text-white",
-      error: "bg-red-400 border-l-4 border-red-700 text-black",
+      notice:   "border-l-4 border-primary-900 bg-primary-600 text-white",
+      error:    "bg-red-400 border-l-4 border-red-700 text-black",
+      info:     "bg-yellow-300 border-l-4 border-yellow-400 text-black"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
