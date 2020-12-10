776ca53a90551ed34619422b846e3baf5bffcc56
David <david.lobry96@gmail.com>
2020-12-10 22:19:28 +0100

    last_name and first_name aren't on sign in forms and aren't required except on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # :confirmable (disabled devise confirmation)
 
-  validates_presence_of :first_name, :last_name
+  validates :first_name, presence: true, on: :update
   validates :email,
             presence: true,
             uniqueness: true,
--- db/migrate/20201130131325_devise_create_users.rb
+++ db/migrate/20201130131325_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :first_name,          null: false
-      t.string :last_name,           null: false
+      t.string :first_name
+      t.string :last_name,                      :default
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
