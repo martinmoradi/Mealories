d87e5874eb39ce038501eb2b8ee26ca5171dd123
Martin Moradi <moradi.martin@gmail.com>
2020-12-10 23:37:12 +0100

    fix notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def tailwind_classes_for(flash_type)
     {
-      notice: 'bg-primary-600 border-l-4 border-primary-900 text-white',
-      error:  'bg-red-400 border-l-4 border-red-700 text-black',
-      info:   'bg-yellow-300 border-l-4 border-yellow-400 text-black',
-      alert:   'bg-yellow-300 border-l-4 border-yellow-400 text-black'
+      notice: "bg-primary-600 border-l-4 border-primary-900 text-white",
+      error: "bg-red-400 border-l-4 border-red-700 text-black",
+      info: "bg-yellow-300 border-l-4 border-yellow-400 text-black",
+      alert: "bg-yellow-300 border-l-4 border-yellow-400 text-black",
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
   def tailwind_classes_redo(type)
     case type
     when "notice"
-      'bg-primary-600 border-l-4 border-primary-900 text-white'
+      "bg-primary-600 border-l-4 border-primary-900 text-white"
     when "error"
       "bg-red-400 border-l-4 border-red-700 text-black"
     when "info" || "alert"
@@ -21,10 +21,10 @@ module ApplicationHelper
 
   def tailwind_text_for(flash_type)
     {
-      notice: 'text-white',
-      error:  'text-black',
-      info:   'text-black',
-      alert:   'text-black'
+      notice: "text-white",
+      error: "text-black",
+      info: "text-black",
+      alert: "text-black",
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
