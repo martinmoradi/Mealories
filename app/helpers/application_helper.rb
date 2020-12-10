45fda739a863a2a86f49d5496d08a7e0b11912fb
Martin Moradi <moradi.martin@gmail.com>
2020-12-10 23:32:10 +0100

    fix notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,17 @@ module ApplicationHelper
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
+  def tailwind_classes_redo(type)
+    case type
+    when "notice"
+      'bg-primary-600 border-l-4 border-primary-900 text-white'
+    when "error"
+      "bg-red-400 border-l-4 border-red-700 text-black"
+    when "info" || "alert"
+      "bg-yellow-300 border-l-4 border-yellow-400 text-black"
+    end
+  end
+
   def tailwind_text_for(flash_type)
     {
       notice: 'text-white',
