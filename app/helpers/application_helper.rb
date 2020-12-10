5a79bec7a3627d397aeb188c69c4ef10ea1241e9
Martin Moradi <moradi.martin@gmail.com>
2020-12-10 23:47:14 +0100

    fix notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def tailwind_text_for(flash_type)
-    {
-      notice: "text-white",
-      error: "text-black",
-      info: "text-black",
-      alert: "text-black",
-    }.stringify_keys[flash_type.to_s] || flash_type.to_s
-  end
 end
