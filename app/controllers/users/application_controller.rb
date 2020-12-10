b66432d2f8769bc00330b23356e6b38f7a035c20
Martin Moradi <moradi.martin@gmail.com>
2020-12-10 23:00:02 +0100

    fix notices

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -4,8 +4,6 @@ class Users::ApplicationController < ApplicationController
 
   def user_profile_incomplete
     if current_user.incomplete_profile?
-      redirect_to edit_user_path(current_user.id)
-      flash[:notice]="Merci de renseigner tes informations pour continuer"
-    end
+      redirect_to edit_user_path(current_user.id) { notice: "Merci de renseigner tes informations pour continuer" }   end
   end
 end
