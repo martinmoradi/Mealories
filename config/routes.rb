a671a6acd8bfe0475d295921b44d6cd4670815ff
Martin Moradi <moradi.martin@gmail.com>
2020-12-10 22:28:53 +0100

    fixing merge

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,7 @@ Rails.application.routes.draw do
     resources :admins
     resources :recipes, only: [:show, :create, :new]
   end
-<<<<<<< HEAD
         
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
-=======
-
->>>>>>> 37d0f23544839f5ce36932a33707cbf73af3bad1
 end
