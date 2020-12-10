563bf935513af6948244aa1cfbc56f0ee6b46eea
David <david.lobry96@gmail.com>
2020-12-10 21:10:22 +0100

    404 page error added

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Mealories
     # https://stackoverflow.com/questions/57277351/rails-6-zeitwerknameerror-doesnt-load-class-from-module
     config.autoloader = :classic
     config.i18n.default_locale = :fr
+    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
