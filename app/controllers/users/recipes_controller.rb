68bcec607094b921088968c7014e330b0b82c89f
Uriel <uriel.sautron@gmail.com>
2020-12-11 15:48:12 +0100

    Clear plan recipe

--- app/controllers/users/recipes_controller.rb
+++ app/controllers/users/recipes_controller.rb
@@ -1,30 +1,16 @@
 class Users::RecipesController < Users::ApplicationController
-  before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :set_recipe, only: %i[show index]
 
-  # GET /recipes
-  # GET /recipes.json
   def index
     @recipes = Recipe.all
   end
 
-  # GET /recipes/1
-  # GET /recipes/1.json
   def show; end
-
-  # GET /recipes/new
-  def new
-    @recipe = Recipe.new
-  end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
-  # def recipe_params
-  #   params.fetch(:recipe, {})
-  # end
 end
